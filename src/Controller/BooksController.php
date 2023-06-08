94b1c95af322c2199ba77992373455c2d638c5ae
Eugene Kvasnyi <eugene.kwasny@gmail.com>
2023-06-08 23:49:25 +0300

    Add index & list controllers

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -2,18 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BooksController extends AbstractController
 {
-    #[Route('/', name: 'app_books')]
-    public function index(): JsonResponse
+    public function __construct(
+       private  EntityManagerInterface $entityManager,
+       private  BookRepository $bookRepository
+    )
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BooksController.php',
+        
+    }
+
+    #[Route('/index', name: 'app_books_index')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(BookType::class, new Book());
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->entityManager->persist($form->getData());
+            $this->entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+
+            return $this->redirectToRoute('app_books_index');
+        }
+
+        return $this->render('book/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/list', name: 'app_books_list')]
+    public function list()
+    {
+        return $this->render('book/list.html.twig', [
+            'books' => $this->bookRepository->findAll()
         ]);
     }
 }
